46894bf6c0b96e82c4bf6a05bdc1323ba180bec6
binnear <binnear@163.com>
2018-10-15 23:29:17 +0800

    text

--- src/turntable/turntable.jsx
+++ src/turntable/turntable.jsx
@@ -1,15 +1,30 @@
+const noPrize = 'https://user-gold-cdn.xitu.io/2018/10/15/16677fb978eb1538?w=128&h=128&f=png&s=6861'
+
 export default class Turntable {
   constructor(options) {
     this.canvas = options.canvas
     this.context = options.context
     this.awards = [
-      {level: '特等奖', name: '我的亲笔签名', color: '#576c0a'},
-      {level: '未中奖', name: '未中奖', color: '#ad4411'},
-      {level: '一等奖', name: '玛莎拉蒂跑车', color: '#43ed04'},
-      {level: '未中奖', name: '未中奖', color: '#d5ed1d'},
-      {level: '二等奖', name: '辣条一包', color: '#32acc6'},
-      {level: '未中奖', name: '未中奖', color: '#e06510'},
+      { level: '特等奖', name: '我的亲笔签名', color: '#576c0a' },
+      { level: '未中奖', name: '未中奖', color: '#ad4411' },
+      { level: '一等奖', name: '玛莎拉蒂超级经典限量跑车', color: '#43ed04' },
+      { level: '未中奖', name: '未中奖', color: '#d5ed1d' },
+      { level: '二等奖', name: '辣条一包', color: '#32acc6' },
+      { level: '未中奖', name: '未中奖', color: '#e06510' },
     ]
+  }
+  getLineTextList(context, text, maxLineWidth) {
+    let wordList = text.split(''), tempLine = '', lineList = []
+    for (let i = 0; i < wordList.length; i++) {
+      if (context.measureText(tempLine).width >= maxLineWidth) {
+        lineList.push(tempLine)
+        maxLineWidth -= context.measureText(text[0]).width
+        tempLine = ''
+      }
+      tempLine += wordList[i]
+    }
+    lineList.push(tempLine)
+    return lineList
   }
   drawPanel() {
     const context = this.context
@@ -34,17 +49,16 @@ export default class Turntable {
       context.restore()
 
       context.save();
-			context.fillStyle = '#FFFFFF';
-			context.font = "24px Arial";
-			context.translate(
-				150 + Math.cos(startRadian + RadianGap / 2) * 140,
-				150 + Math.sin(startRadian + RadianGap / 2) * 140
-			);
-			context.rotate(startRadian + RadianGap / 2 + Math.PI / 2);
-			// this.getLineTextArr(context, awards, 60).forEach((data, index) => {
-				
-      // })
-      context.fillText(awards[i].name, -context.measureText(awards[i].name).width / 2, 40);
+      context.fillStyle = '#FFFFFF';
+      context.font = "14px Arial";
+      context.translate(
+        150 + Math.cos(startRadian + RadianGap / 2) * 140,
+        150 + Math.sin(startRadian + RadianGap / 2) * 140
+      );
+      context.rotate(startRadian + RadianGap / 2 + Math.PI / 2);
+      this.getLineTextList(context, awards[i].name, 70).forEach((line, index) => {
+        context.fillText(line, -context.measureText(line).width / 2, ++index * 25);
+      })
       context.restore();
 
       startRadian += RadianGap
